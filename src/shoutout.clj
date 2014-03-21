1e6f8f77ca8060ff7a5344cc3e89a4f6b0556fb8
Tom Crayford <tcrayford@googlemail.com>
2014-03-21 12:35:59 +0000

    implement parsing code

--- src/shoutout.clj
+++ src/shoutout.clj
@@ -1,6 +1,22 @@
-(ns shoutout)
+(ns shoutout
+  (:require [clojure.string :as s]))
 
 (defrecord Feature [feature-name groups users percentage])
 
-(defn parse-feature [^String raw]
-  )
+(defn split [raw pattern]
+  (if (empty? raw)
+    []
+    (s/split raw pattern)))
+
+(defn parse-percentage [^String raw]
+  (if (empty? raw)
+    0
+    (Integer/parseInt raw)))
+
+(defn parse-feature [^String feature-name ^String raw]
+  (let [[raw-percentage raw-users raw-groups]
+        (split raw #"\|")]
+    (Feature. feature-name
+              (into [] (split raw-groups #","))
+              (into [] (split raw-users  #","))
+              (parse-percentage raw-percentage))))
--- test/shoutout/core_test.clj
+++ test/shoutout/core_test.clj
@@ -1,6 +1,39 @@
 (ns shoutout.core-test
   (:require [clojure.test :refer :all]
-            [shoutout :refer :all]))
+            [shoutout :refer :all :reload true]))
 
 (deftest parse-feature-test
-  ())
+  (testing "groups"
+    (testing "an empty feature has no groups"
+      (is (= (:groups (parse-feature "an-feature" "||"))
+             [])))
+
+    (testing "an feature can have an group"
+      (is (= (:groups (parse-feature "an-feature" "||admin"))
+             ["admin"])))
+
+    (testing "an feature can have an groups"
+      (is (= (:groups (parse-feature "an-feature" "||admin,client"))
+             ["admin" "client"]))))
+
+  (testing "users"
+    (testing "an empty feature has no users"
+      (is (= (:users (parse-feature "an-feature" "||"))
+             [])))
+
+    (testing "an feature can have an user"
+      (is (= (:users (parse-feature "an-feature" "|1|"))
+             ["1"])))
+
+    (testing "an feature can have an users"
+      (is (= (:users (parse-feature "an-feature" "|1,2,3|"))
+             ["1" "2" "3"]))))
+
+  (testing "percentage"
+    (testing "an empty feature has a percentage of 0"
+      (is (= (:percentage (parse-feature "an-feature" "||"))
+             0)))
+
+    (testing "an feature can have an percentage"
+      (is (= (:percentage (parse-feature "an-feature" "89||"))
+             89)))))
