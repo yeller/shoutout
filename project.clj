21bdc9d835b873c1b9ab812ae9e2d54e44925491
Tom Crayford <tcrayford@googlemail.com>
2014-03-22 13:22:00 +0000

    fix description in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject shoutout "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Feature flags for clojure"
+  :url "https://github.com/tcrayford/shoutout"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
