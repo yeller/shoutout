146201ead186b72c5b61f201faeae349ef04b439
Tom Crayford <tcrayford@googlemail.com>
2014-03-22 12:36:08 +0000

    fix up project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.namespace "0.2.4"]
-                 ])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]}})
